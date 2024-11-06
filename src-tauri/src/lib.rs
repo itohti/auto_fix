888ebbf7a4241fad66ff2f18dd622c272d0932f3
Izdar Tohti <itohti@gmail.com>
2024-11-06 17:20:52 -0500

    frontend looking like previous app and it reads docx files

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,69 @@
+use dotext::{Docx, MsDoc};
+use std::io::{self, Read};
+use std::path::Path;
+use std::fs;
+
+/* Helper functions */
+fn read_docx(path: &Path) -> std::io::Result<String>{
+    let mut content = String::new();
+    let mut docx = Docx::open(path)?;
+    let bytes_read = docx.read_to_string(&mut content)?;
+
+    Ok(content)
+}
+
+fn get_file_content(path: &Path) -> std::io::Result<String> {
+    match path.extension().and_then(|ext| ext.to_str()) {
+        Some("docx") => {
+            let content = read_docx(path)?;
+            return Ok(content);
+        } 
+        Some(ext) => {
+            return Err(std::io::Error::new(std::io::ErrorKind::Other, format!("Unsupported file type! {}", ext)));
+        }
+        None => {
+            return Err(std::io::Error::new(std::io::ErrorKind::Other, format!("No file extension found!")));
+        }
+    }
+}
+
+#[tauri::command]
+fn auto_fix(path: &Path) -> Result<String, String>{
+    println!("Hi from auto_fix I got path {:?}", path);
+    let content = get_file_content(path).map_err(|e| format!("Could not get file content with this error {}", e))?;
+    Ok(content)
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            auto_fix
+        ])
+        .plugin(tauri_plugin_dialog::init())
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn read_docx_test() {
+        let source_path = Path::new("./tests_files/Requirmenment for Editing Tool - My Project.docx");
+        let result = read_docx(source_path);
+
+        assert!(result.is_ok(), "Expected Ok but got error {}", result.unwrap_err());
+    }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { invoke } from "@tauri-apps/api/core";
+    import { open } from "@tauri-apps/plugin-dialog";
+    let isFileLoaded = false;
+    let auto_fixed_content = "";
+
+    async function handleFileUpload() {
+        try {
+            const filePath = await open({
+                multiple: false,
+                filters: [
+                    { name: 'All Files', extensions: ['docx', 'txt'] },
+                ]
+            });
+
+            if (filePath) {
+                isFileLoaded = true;
+                auto_fixed_content = await invoke("auto_fix", {path: filePath});
+            }
+        } catch (error) {
+            console.error('Error opening file dialog:', error);
+        }
+    }
+</script>
+
+<div class="flex flex-col h-screen">
+    <div class="flex gap-2 p-2">
+        <button 
+            class="px-4 border border-sky-500 rounded-sm hover:bg-sky-500 hover:text-white transition-colors"
+        >
+            Auto-fix
+        </button>
+        <button
+            class="px-4 border border-sky-500 rounded-sm hover:bg-sky-500 hover:text-white transition-colors"
+            on:click={handleFileUpload}
+        >
+            Load File
+        </button>
+    </div>
+
+    <div class="grid grid-cols-2 gap-1 flex-1 overflow-hidden">
+        <div class="h-full border overflow-auto">
+            {#if isFileLoaded}
+                <pre class="whitespace-pre-wrap p-4 font-mono text-sm break-words">{auto_fixed_content}</pre>
+            {:else}
+                <div class="h-full flex items-center justify-center">
+                    <p>No File Uploaded</p>
+                </div>
+            {/if}
+        </div>
+
+        <div class="border p-4">
+            <p>Output</p>
+        </div>
+    </div>
+</div>
